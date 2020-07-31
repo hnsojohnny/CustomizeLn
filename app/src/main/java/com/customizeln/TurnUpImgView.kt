5e47242e466cd1294246dcd9e85e9dd787d01718
hnsojohnny <13522399301@163.com>
2020-07-31 14:06:04 +0800

    提交翻页

--- app/src/main/java/com/customizeln/TurnUpImgView.kt
+++ app/src/main/java/com/customizeln/TurnUpImgView.kt
@@ -17,36 +17,36 @@ private val PADDING = 200f.dp2px
 class TurnUpImgView constructor(context: Context, attr: AttributeSet?) : View(context, attr){
 
     constructor(context: Context): this(context, null)
-    val bitmap = getImg()
-    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    val camera = Camera()
+    private val bitmap = getImg()
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val camera = Camera()
     init {
         camera.setLocation(0f, 0f, -20 * resources.displayMetrics.density)
-        camera.rotateX(50f)
+        camera.rotateX(45f)
     }
 
     override fun onDraw(canvas: Canvas) {
         canvas.save()
-        canvas.translate(PADDING + IMG_WIDTH/2, PADDING+IMG_WIDTH/2)
-        canvas.rotate(-45f)
-        canvas.clipRect(PADDING, PADDING, PADDING + IMG_WIDTH, PADDING + IMG_WIDTH/2)
-        canvas.rotate(45f)
+        canvas.translate(PADDING + IMG_WIDTH/2, PADDING + IMG_WIDTH/2)
+        canvas.rotate(-30f)
+        canvas.clipRect(-IMG_WIDTH, -IMG_WIDTH, IMG_WIDTH, 0f)
+        canvas.rotate(30f)
         canvas.translate(-(PADDING + IMG_WIDTH/2), -(PADDING + IMG_WIDTH/2))
         canvas.drawBitmap(bitmap, PADDING, PADDING, paint)
         canvas.restore()
 
         canvas.save()
-        canvas.translate(PADDING + IMG_WIDTH/2, PADDING+IMG_WIDTH/2)
-        canvas.rotate(-45f)
+        canvas.translate(PADDING + IMG_WIDTH/2, PADDING + IMG_WIDTH/2)
+        canvas.rotate(-30f)
         camera.applyToCanvas(canvas)
-        canvas.clipRect(PADDING, PADDING + IMG_WIDTH/2, PADDING + IMG_WIDTH, PADDING + IMG_WIDTH)
-        canvas.rotate(45f)
+        canvas.clipRect(-IMG_WIDTH, 0f, IMG_WIDTH, IMG_WIDTH)
+        canvas.rotate(30f)
         canvas.translate(-(PADDING + IMG_WIDTH/2), -(PADDING + IMG_WIDTH/2))
         canvas.drawBitmap(bitmap, PADDING, PADDING, paint)
         canvas.restore()
     }
 
-    fun getImg(): Bitmap{
+    private fun getImg(): Bitmap{
         val options =  BitmapFactory.Options()
         options.inJustDecodeBounds = true
         BitmapFactory.decodeResource(resources, R.drawable.lion, options)
